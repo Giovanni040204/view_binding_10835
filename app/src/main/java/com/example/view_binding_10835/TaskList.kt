3cdeee457ddc853f0ef8d099c8848a7679aa9eff
Giovanni040204 <giovannitemaluru00@gmail.com>
2022-09-15 08:39:47 +0700

    Initial commit

---
+++ app/src/main/java/com/example/view_binding_10835/TaskList.kt
@@ -0,0 +1,11 @@
+package com.example.view_binding_10835
+
+object TaskList {
+    val taskList = listOf<Task> (
+        Task("Chapter 1", "Awal Mulanya", "Semuanya dimulai dari sini"),
+        Task("Chapter 2", "Tatapan Pertama", "Saat melihat dia, aku tidak bisa melupakannya"),
+        Task("Chapter 3", "Senyuman", "Seolah-olah aku adalah manusia yang paling beruntung"),
+        Task("Chapter 4", "Kedua Kalinya", "Semuanya sama seperti sebelumnya, tapi aku menikmatinya"),
+        Task("Chapter 5", "Lokasi", "Sekitarku menjadi saksi bisu atas apa yang terjadi")
+    )
+}
